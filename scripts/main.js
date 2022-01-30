59dda866b7e7676989c0390239dd0ba432958402
thatGui <gui@thatgui.com>
2022-01-30 18:33:41 +0000

    full screen canvas

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,49 @@
-// global variables
+/* global variables */
 const canvas = document.getElementById('gameCanvas');
-const ctx = 
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let ship;
+let speed;
+let angle;
+
+let squares = [];
+let octagon = [];
+let pentagon = [];
+let lasers = [];
+
+/* game assets - these will be moved onto classes.js file */
+class Ship {
+    constructor(){
+
+    }
+};
+
+class Squares {
+    constructor(){
+
+    }
+};
+
+/* game logic */
+class Game {
+    constructor(){
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.width = canvas.width;
+        this.height = canvas.height;
+        this.intervalId = null;
+    }
+};
+
+/* game start */
+window.onload = () => {
+    document.getElementById('startbtn').onclick = () => {
+        startGame();
+    };
+    
+    function startGame() {
+        const game = new Game();
+        game.start();
+    };
+};
