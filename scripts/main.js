b734bf764e7bf83011e137f2f7d3f6983adac64f
thatGui <gui@thatgui.com>
2022-01-31 18:24:51 +0000

    rocket fuel and drag fixed

--- scripts/main.js
+++ scripts/main.js
@@ -20,12 +20,13 @@ class Ship {
         this.canvas = canvas;
         this.x = canvas.width / 2;
         this.y = canvas.height / 2;
-        this.speed = 0;
+        this.speed = 0.3;
         this.angle = 90 / 180 * Math.PI;
         this.radius = 32;
         this.rotation = 0;
-        this.velocityX = 0;
-        this.velocityY = 0;
+        this.rocketFuel = false;
+        this.rocketFuelX = 0;
+        this.rocketFuelY = 0;
     }
     draw(){
         ctx.strokeStyle = '#008080';
@@ -37,16 +38,16 @@ class Ship {
         ctx.fill(); */
         ctx.beginPath(); // formula for Equilateral triangle provided by freeCodeCamp
             ctx.moveTo( // tip of the triangle
-                this.x + 4 / 3 * this.radius * Math.cos(this.angle),
-                this.y - 4 / 3 * this.radius * Math.sin(this.angle)
+                this.x + 1.1 * this.radius * Math.cos(this.angle),
+                this.y - 1.1 * this.radius * Math.sin(this.angle)
             );
             ctx.lineTo( // bottom left triangle corner point
-                this.x - this.radius * (2 / 3 * Math.cos(this.angle) + Math.sin(this.angle)),
-                this.y + this.radius * (2 / 3 * Math.sin(this.angle) - Math.cos(this.angle))
+                this.x - this.radius * (0.6 * Math.cos(this.angle) + Math.sin(this.angle)),
+                this.y + this.radius * (0.6 * Math.sin(this.angle) - Math.cos(this.angle))
             );
             ctx.lineTo( // bottom right triangle corner point
-                this.x - this.radius * (2 / 3 * Math.cos(this.angle) - Math.sin(this.angle)),
-                this.y + this.radius * (2 / 3 * Math.sin(this.angle) + Math.cos(this.angle))
+                this.x - this.radius * (0.6 * Math.cos(this.angle) - Math.sin(this.angle)),
+                this.y + this.radius * (0.6 * Math.sin(this.angle) + Math.cos(this.angle))
             );
             ctx.closePath();
             ctx.fill();
@@ -56,7 +57,30 @@ class Ship {
         this.angle += this.rotation;
     }
     update(){
-
+        this.x += this.rocketFuelX;
+        this.y += this.rocketFuelY;
+        if(this.rocketFuel) {
+            this.rocketFuelX += this.speed * Math.cos(this.angle);
+            this.rocketFuelY -= this.speed * Math.sin(this.angle);
+            
+            ctx.fillStyle = '#ffaa00';
+            ctx.beginPath();
+            ctx.moveTo( 
+                this.x - this.radius * (0.6 * Math.cos(this.angle) + 0.5 * Math.sin(this.angle)),
+                this.y + this.radius * (0.6 * Math.sin(this.angle) - 0.5 * Math.cos(this.angle))
+            );
+            ctx.lineTo( 
+                this.x - this.radius * 1.1 * Math.cos(this.angle),
+                this.y + this.radius * 1.1 * Math.sin(this.angle)
+            );
+            ctx.lineTo( 
+                this.x - this.radius * (0.6 * Math.cos(this.angle) - 0.5 * Math.sin(this.angle)),
+                this.y + this.radius * (0.6 * Math.sin(this.angle) + 0.5 * Math.cos(this.angle))
+            );
+            ctx.closePath();
+            ctx.fill();
+           
+        }
     }
 };
 
@@ -80,6 +104,7 @@ class Game {
     start(){
         this.ship = new Ship();
         this.keyboardEvent();
+        this.gameOver();
         this.intervalId = setInterval( () => {
             this.update();
         }, 1000 / 60);
@@ -87,20 +112,21 @@ class Game {
     update(){
         this.ctx.clearRect(0, 0, canvas.width, canvas.height);
         this.ship.draw();
+        this.ship.update();
         this.createEnemies();
         
         
         
-        /* if (ship.x < 0 - ship.radius) {
-            ship.x = canvas.width + ship.radius;
-        } else if (ship.x > canvas.width + ship.radius) {
-            ship.x = 0 - ship.radius;
+        if (this.ship.x < 0 - this.ship.radius) {
+            this.ship.x = canvas.width + this.ship.radius;
+        } else if (this.ship.x > canvas.width + this.ship.radius) {
+            this.ship.x = 0 - this.ship.radius;
         }
-        if (ship.y < 0 - ship.radius) {
-            ship.y = canvas.height + ship.radius;
-        } else if (ship.y > canvas.height + ship.radius) {
-            ship.y = 0 - ship.radius;
-        } */
+        if (this.ship.y < 0 - this.ship.radius) {
+            this.ship.y = canvas.height + this.ship.radius;
+        } else if (this.ship.y > canvas.height + this.ship.radius) {
+            this.ship.y = 0 - this.ship.radius;
+        }
     }
     createEnemies(){
         const squares = new Squares();
@@ -108,15 +134,26 @@ class Game {
     keyboardEvent(){
         window.addEventListener('keydown', (e) => {
             switch (e.code) {
-                case 'ArrowRight': this.ship.angle -= 0.3;
+                case 'ArrowRight': this.ship.angle -= 0.5;
                     break;
-                case 'ArrowLeft': this.ship.angle += 0.3;
+                case 'ArrowLeft': this.ship.angle += 0.5;
                     break;
-                case 'ArrowUp': this.ship.y -= 10;
+                case 'ArrowUp': this.ship.rocketFuel = true;
                     break;
                 
             }
         })
+        window.addEventListener('keyup', (e) => {
+            switch (e.code) {
+                case 'ArrowUp': this.ship.rocketFuel = false;
+            console.log('up');
+                    break;
+                
+            }
+        })
+    }
+    gameOver(){
+
     }
 };
 
